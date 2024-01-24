d300851cd95656f48e76cb6f04e49d41f625b712
Tiago061 <Thenriqueleite277@gmail.com>
2024-01-23 21:13:23 -0300

    Criado a Classe de Reserva

---
+++ src/entities/Reservation.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class Reservation {
+
+    private int id;
+    private String name;
+    private String date;
+    private int numberOfGuests;
+
+    public Reservation(){
+
+    }
+    public Reservation(int id, String name, String date, int numberOfGuests) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+}
